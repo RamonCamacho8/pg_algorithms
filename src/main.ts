5f7f68ee9c12b89236ab6a35901233887c75bb6e
Arce <113393688+DevArce@users.noreply.github.com>
2025-03-10 21:56:32 -0700

    add tiles canvas to HTML, implement tile extraction and rendering in p5.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import './style.css'
 import P5 from 'p5'
-import { renderImage } from './utils/utils';
+import { renderImage, extractTiles, renderTiles } from './utils/utils';
 
 
-const sketch = (p5 : P5) => {
+const drawingCanvas = (p5 : P5) => {
     let img: P5.Image;
-    const CANVAS_SIZE  = 300;
+    const CANVAS_SIZE  = 200;
 
     p5.preload = () => {
       img = p5.loadImage('/assets/test_1.png');
@@ -25,7 +25,34 @@ const sketch = (p5 : P5) => {
     p5.draw = () => {}
 }
 
-new P5(sketch)
+new P5(drawingCanvas)
+
+
+const displayTilesCanvas = (p5 : P5) => {
+
+  const CANVAS_SIZE = 200;
+
+  let img : P5.Image;
+  let tiles : P5.Image[];
+
+  p5.preload = () => {
+    img = p5.loadImage('/assets/test_1.png');
+  }
+
+  p5.setup = () => {
+    const canvas = p5.createCanvas(CANVAS_SIZE, CANVAS_SIZE)
+    canvas.parent('tiles-canvas')
+    p5.background(0)
+    tiles = extractTiles(p5, img, 3)
+    console.log('tiles', tiles)
+    renderImage(p5, tiles[3], 0, 0, CANVAS_SIZE)
+  }
+
+  p5.draw = () => {}
+
+}
+
+new P5(displayTilesCanvas)
 
 
 
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,27 +1,69 @@
 import P5 from "p5";
 
-export const renderImage = (p5: P5, img: P5.Image, posX : number, posY : number, canvas_size : number) => {
-    
+export const renderImage = (
+    p5: P5,
+    img: P5.Image,
+    posX: number,
+    posY: number,
+    canvas_size: number
+) => {
     img.loadPixels();
-    
+
     // Assuming is a square image, size in pixels.
-    const img_size = img.width;
-    const square_size = canvas_size / img_size;
+    const imgSize = img.width;
+    const squareSize = canvas_size / imgSize;
 
     p5.push();
 
-    for (let y = 0; y < img_size; y++) {
-        for (let x = 0; x < img_size; x++) {
-            const index = (x + y * img_size) * 4;
+    for (let y = 0; y < imgSize; y++) {
+        for (let x = 0; x < imgSize; x++) {
+            const index = (x + y * imgSize) * 4;
             const r = img.pixels[index];
             const g = img.pixels[index + 1];
             const b = img.pixels[index + 2];
             const a = img.pixels[index + 3];
             p5.fill(r, g, b, a);
-            p5.square(x * square_size + posX, y * square_size + posY, square_size);
+            p5.square(x * squareSize + posX, y * squareSize + posY, squareSize);
         }
     }
 
     p5.pop();
+};
 
-}
+export const renderTiles = (p5 : P5, tiles : P5.Image[], canvas_size : number) => {
+
+    const imgSize = tiles[0].width;
+    const squareSize = canvas_size / imgSize;
+    console.log('squareSize', squareSize);
+    const tilesNumber = tiles.length;
+
+    for (let i = 0; i < tilesNumber; i++) {
+        const x = i % imgSize;
+        const y = Math.floor(i / imgSize);
+        p5.image(tiles[i], x * squareSize, y * squareSize, squareSize, squareSize);
+    }
+
+
+};
+
+export const extractTiles = (
+    p5: P5,
+    sourceImage: P5.Image,
+    tileSize: number
+) => {
+    sourceImage.loadPixels();
+
+    const imgSize = sourceImage.width;
+    let tiles : P5.Image[] = [];
+   
+
+    for (let i = 0; i < imgSize; i++) {
+        for (let j = 0; j < imgSize; j++) {
+            const tileImage = p5.createImage(tileSize, tileSize);
+            tileImage.copy(sourceImage, i, j, tileSize, tileSize, 0, 0, tileSize, tileSize);
+            tiles.push(tileImage);
+        }
+    }
+
+    return tiles;
+};
