1994bae0168ab0f56f7a0faf0590f525d0900068
Arce <113393688+DevArce@users.noreply.github.com>
2025-03-10 21:21:00 -0700

    add image loading functionality in p5.js sketch and include test image asset

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,18 @@ import P5 from 'p5'
 
 
 const sketch = (p5 : P5) => {
+    let img: P5.Image;
+
+    p5.preload = () => {
+      img = p5.loadImage('/assets/test_1.jpg');
+    }
 
   
     p5.setup = () => {
         const canvas = p5.createCanvas(400, 400)
         canvas.parent('app')
         p5.background(0)
-
+        p5.image(img, 0, 0)
     }
     p5.draw = () => {}
 }
